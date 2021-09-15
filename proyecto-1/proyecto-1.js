cc5db9ded920e969bded2c111724af5108567612
Leo Distante <leoneldistante@gmail.com>
2021-09-15 17:26:59 -0300

    first commit

---
+++ proyecto-1/proyecto-1.js
@@ -0,0 +1,8 @@
+'use strict'
+/*
+programa para pedir dos numeros y señalar mayor, menor o iguales
+*/
+var numero1 = parseInt(prompt ('introduce el primer numero', 0));
+var numero2 = parseInt(prompt ('introduce el segundo numero', 0));
+
+console.log(numero1, numero2);
